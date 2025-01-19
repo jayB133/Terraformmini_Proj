534831b2cd99d30bc94058bd07c96643deadba1c
Ubuntu <ubuntu@ubuntu.ht.home>
2025-01-19 19:28:39 +0000

    Updated README and commands

--- commands/m4_commands.sh
+++ commands/m4_commands.sh
@@ -12,8 +12,8 @@ terraform plan -var=billing_code="ACCT8675309" -var=project="web-app" -var=aws_a
 
 # And we can store our sensitive data in environment variables like so
 # For Linux and MacOS
-export TF_VAR_aws_access_key=YOUR_ACCESS_KEY
-export TF_VAR_aws_secret_key=YOUR_SECRET_KEY
+export TF_VAR_aws_access_key="YOUR_ACCESS_KEY"
+export TF_VAR_aws_secret_key="YOUR_SECRET_KEY"
 
 # For PowerShell
 $env:TF_VAR_aws_access_key="YOUR_ACCESS_KEY"
